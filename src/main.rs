66d820aa66144eeb167473c55b64b0ddef871288
Blintx40 <114827960+blintx@users.noreply.github.com>
2024-09-02 22:53:59 +0200

    initial database setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use rusqlite::Connection;
+
 fn main() {
-    println!("Hello, world!");
+    let conn= Connection::open("./db.db3").unwrap();
+    
+    setup_db(conn);
+
+    println!("Hello, világ!");
 }
+
+fn setup_db(conn:Connection){
+
+    conn.execute("CREATE TABLE IF NOT EXISTS csengo (id INTEGER PRIMARY KEY, time TEXT NOT NULL, path TEXT NOT NULL)", ()).expect("Nem sikerült létrehozni a táblát");
+
+}
+
+
