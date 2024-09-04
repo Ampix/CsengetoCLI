a2d488f6982ac9be5fb14abf977c2e49a97fe19b
Blintx40 <114827960+blintx@users.noreply.github.com>
2024-09-04 21:37:25 +0200

    Input

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use rusqlite::Connection;
+use std::io;
 
 fn main() {
     let conn= Connection::open("./db.db3").unwrap();
     
     setup_db(conn);
+    let mut condition :bool = true;
+    while condition {
+        println!("Hello, világ!");
+        let mut input_string  = String::new();
+        io::stdin().read_line(&mut input_string).expect("Failed to read line");
+        println!("Input {}",input_string);
 
+        condition = false;
+    }
     println!("Hello, világ!");
+
 }
 
 fn setup_db(conn:Connection){
