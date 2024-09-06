744d227fa84e8565b57d3ab416d7ad87b34f2de5
Blintx40 <114827960+blintx@users.noreply.github.com>
2024-09-06 22:43:49 +0200

    get_data function

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,52 @@
 use rusqlite::Connection;
-use std::io;
 
+#[derive(Debug)]
+struct Hangok {
+    id: i32,
+    time: String,
+    path: String,
+    status: i8,
+}
 fn main() {
-    let conn= Connection::open("./db.db3").unwrap();
-    
-    setup_db(conn);
-    let mut condition :bool = true;
-    while condition {
-        println!("Hello, világ!");
-        let mut input_string  = String::new();
-        io::stdin().read_line(&mut input_string).expect("Failed to read line");
-        println!("Input {}",input_string);
+    let conn = Connection::open("./db.db3").unwrap();
 
-        condition = false;
+    setup_db(&conn);
+
+    let hangok = get_data(&conn);
+    conn.close().expect("Valahogy nem sikerült bezárni");
+
+    for hang in hangok {
+        println!(
+            "id:{} time:{} path:{} status:{}",
+            hang.id, hang.time, hang.path, hang.status
+        )
     }
-    println!("Hello, világ!");
-
 }
 
-fn setup_db(conn:Connection){
-
-    conn.execute("CREATE TABLE IF NOT EXISTS csengo (id INTEGER PRIMARY KEY, time TEXT NOT NULL, path TEXT NOT NULL)", ()).expect("Nem sikerült létrehozni a táblát");
-
+fn setup_db(conn: &Connection) {
+    conn.execute("CREATE TABLE IF NOT EXISTS csengo (id INTEGER PRIMARY KEY, time TEXT NOT NULL, path TEXT NOT NULL,status BOOLEAN DEFAULT 1 NOT NULL   )", ()).expect("Nem sikerült létrehozni a táblát");
 }
 
+fn get_data(conn: &Connection) -> Vec<Hangok> {
+    let mut datas = conn
+        .prepare("SELECT * FROM csengo")
+        .expect("Nem sikerült lekérni az adatokat");
+    let hangok = datas
+        .query_map([], |row| {
+            Ok(Hangok {
+                id: row.get(0)?,
+                time: row.get(1)?,
+                path: row.get(2)?,
+                status: row.get(3)?,
+            })
+        })
+        .unwrap();
 
+    let mut hangok_lista = vec![];
+
+    for hang in hangok {
+        hangok_lista.push(hang.unwrap());
+    }
+
+    hangok_lista
+}
