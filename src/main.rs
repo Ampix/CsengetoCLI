72221f5aa07e74f310e1da101ad0db6f1b187539
Blintx40 <114827960+blintx@users.noreply.github.com>
2024-09-07 18:06:52 +0200

    sorting the hangok by time and making hangok struct cloneble and replacing time to hour an minute

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use chrono::{Local, Timelike};
+
 use rodio::{Decoder, OutputStream, Sink};
 use rusqlite::Connection;
 use std::fs::File;
 use std::io::BufReader;
+
+use std::ops::Index;
 use std::thread;
 use std::time::Duration;
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Hangok {
     id: i32,
-    time: String,
+    hour: i32,
+    minute: i32,
     path: String,
     status: i8,
 }
@@ -18,32 +21,26 @@ fn main() {
 
     setup_db(&conn);
 
-    let hangok = get_data(&conn);
+    let mut hangok = get_data(&conn);
     conn.close().expect("Valahogy nem sikerült bezárni");
+    for hang in &hangok {
+        println!("óra{} perc{} ", hang.hour, hang.minute,)
+    }
+    hangok = buble_sort(hangok.clone());
+    for hang in &hangok {
+        println!("óra{} perc{}", hang.hour, hang.minute)
+    }
+    let condition = true;
 
-    let mut condition = true;
     while condition {
         let now = Local::now().time();
 
-        for hang in &hangok {
-            let mut time_vector: Vec<&str> = hang.time.split(":").collect();
-
-            if let Some(stripped) = time_vector[0].strip_prefix("0") {
-                time_vector[0] = stripped;
-            }
-
-            if let Some(stripped) = time_vector[1].strip_prefix("0") {
-                time_vector[1] = stripped;
-            }
-
+        for hang in hangok.clone() {
             println!("Mostan {} óra {} perc van", now.hour(), now.minute());
-            println!(
-                "Zene {} óra {} perckor lesz ",
-                time_vector[0], time_vector[1]
-            );
+            println!("Zene {} óra {} perckor lesz ", hang.hour, hang.minute);
 
-            if time_vector[0] == format!("{}", now.hour())
-                && time_vector[1] == format!("{}", now.minute())
+            if hang.hour == now.hour().try_into().unwrap()
+                && hang.minute == now.minute().try_into().unwrap()
                 && hang.status == 1
             {
                 println!("idő van");
@@ -67,9 +64,20 @@ fn get_data(conn: &Connection) -> Vec<Hangok> {
         .expect("Nem sikerült lekérni az adatokat");
     let hangok = datas
         .query_map([], |row| {
+            let time: String = row.get(1)?;
+            let mut time_vector: Vec<&str> = time.split(':').collect();
+
+            if let Some(stripped) = time_vector[0].strip_prefix("0") {
+                time_vector[0] = stripped;
+            }
+
+            if let Some(stripped) = time_vector[1].strip_prefix("0") {
+                time_vector[1] = stripped;
+            }
             Ok(Hangok {
                 id: row.get(0)?,
-                time: row.get(1)?,
+                hour: time_vector[0].to_string().parse::<i32>().unwrap(),
+                minute: time_vector[1].to_string().parse::<i32>().unwrap(),
                 path: row.get(2)?,
                 status: row.get(3)?,
             })
@@ -96,4 +104,48 @@ fn play_mp3(file_path: &String) {
     sink.append(source);
 
     sink.sleep_until_end();
+}
+
+fn buble_sort(input: Vec<Hangok>) -> Vec<Hangok> {
+    let mut output = input.clone();
+    let length = output.len();
+    for hang in &output {
+        println!("{}", hang.hour);
+    }
+    for i in 0..length {
+        let mut swapped = false;
+
+        for j in 0..length - i - 1 {
+            if compare_hangok(&output[j], &output[j + 1]) {
+                swapped = true;
+                println!("swapped");
+                let temp = output[j].clone();
+                output[j] = output[j + 1].clone();
+                output[j + 1] = temp.clone();
+                for hang in &output {
+                    println!("{}", hang.hour);
+                }
+            }
+        }
+        if swapped == false {
+            break;
+        }
+        for hang in &input {
+            println!("{}", hang.hour);
+        }
+    }
+    output
+}
+fn compare_hangok(hang_1: &Hangok, hang_2: &Hangok) -> bool {
+    if hang_1.hour > hang_2.hour {
+        true
+    } else if hang_2.hour == hang_1.hour {
+        if hang_1.minute > hang_2.minute {
+            true
+        } else {
+            false
+        }
+    } else {
+        false
+    }
 }
